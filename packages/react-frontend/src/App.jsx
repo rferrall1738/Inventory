f18c5b5c82fee6bfcf3b9be6260957011f1df2e8
Jacob Lilien <jlilien@calpoly.edu>
2024-12-04 20:35:22 -0800

    Fix merge error

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -26,49 +26,6 @@ function App() {
             </Routes>
         </BrowserRouter>
     );
-
-
-import CreateItem from './CreateItem'
-
-function App() {
- const isAuthenticated = localStorage.getItem('token') ? true : false
- return (
-  <BrowserRouter>
-   <Routes>
-    {/* Public Routes */}
-    <Route
-     path="/"
-     element={<UserLogin />}
-    />{' '}
-    {/* Login page */}
-    <Route
-     path="/login"
-     element={<UserLogin />}
-    />{' '}
-    {/* page */}
-    <Route
-     path="/signup"
-     element={<UserSignup />}
-    />{' '}
-    {/*Signup page */}
-    {/* Private Routes */}
-    <Route element={<ProtectedRoutes isAuthenticated={isAuthenticated} />}>
-     <Route
-      path="/home"
-      element={<HomePage />}
-     />
-     <Route
-      path="/items/:id"
-      element={<Item />}
-     />
-     <Route
-      path="/create-item"
-      element={<CreateItem />}
-     />
-    </Route>
-   </Routes>
-  </BrowserRouter>
- )
 }
 
 export default App
